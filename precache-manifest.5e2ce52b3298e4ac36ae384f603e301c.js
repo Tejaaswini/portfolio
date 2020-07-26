d5433372da412ae52c11d6a67ab3a5ec7b6e8444
Tejaaswini <tejaaswini.narendra@codemonk.in>
2020-07-26 13:54:03 +0530

    Updates

---
+++ precache-manifest.5e2ce52b3298e4ac36ae384f603e301c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c564246f0932ba076d467467c241ff5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "665b9f1f43ba62d0d62e",
+    "url": "/portfolio/static/css/main.23e7c850.chunk.css"
+  },
+  {
+    "revision": "3ca2233b533761eb1c42",
+    "url": "/portfolio/static/js/2.6e6f85f8.chunk.js"
+  },
+  {
+    "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
+    "url": "/portfolio/static/js/2.6e6f85f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "665b9f1f43ba62d0d62e",
+    "url": "/portfolio/static/js/main.9103ea1c.chunk.js"
+  },
+  {
+    "revision": "e0515c236b0c7a4606a3",
+    "url": "/portfolio/static/js/runtime-main.53270146.js"
+  },
+  {
+    "revision": "650820815fdbd5e91e7c989c72bcb85c",
+    "url": "/portfolio/static/media/anime.65082081.jpg"
+  },
+  {
+    "revision": "f4cf21254f42cc47fdc0350aa1b1359d",
+    "url": "/portfolio/static/media/book.f4cf2125.jpg"
+  },
+  {
+    "revision": "78b404aaea0c23882e70cebdd64cf190",
+    "url": "/portfolio/static/media/cat.78b404aa.jpg"
+  },
+  {
+    "revision": "3ddcb5ae755b43e4c0e2da93817105b6",
+    "url": "/portfolio/static/media/contact.3ddcb5ae.svg"
+  },
+  {
+    "revision": "73f5c2ea3da8a08a75c1e7be194c6ba5",
+    "url": "/portfolio/static/media/iiitA.73f5c2ea.png"
+  },
+  {
+    "revision": "9c5575683130c85e9598bd3b9c95a923",
+    "url": "/portfolio/static/media/lion.9c557568.jpg"
+  },
+  {
+    "revision": "503e1cddcc1a80ebf90b12794d6f90d6",
+    "url": "/portfolio/static/media/what_i_know.503e1cdd.svg"
+  }
+]);
